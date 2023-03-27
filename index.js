82734495e2dc08f30af50841d6a100d1176c93d7
Subin Abraham <subinabraham@polusindia.com>
2023-03-27 15:53:40 +0530

    Final Version

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 let myLeads = [];
 const inputSection = document.getElementById("inputField")
 const saveInputSection = document.getElementById("saveInput")
-let listItems = ''
+const data = document.getElementById("unorderedList")
+const deleteItem = document.getElementById("deleteButton")
+const saveURL = document.getElementById("saveUrl")
+let leads = JSON.parse(localStorage.getItem("myLeads"))
+if (leads) {
+    myLeads = leads
+    renderInput(myLeads)
+}
 saveInputSection.addEventListener("click", function () {
     myLeads.push(inputSection.value)
-    renderInput()
-})
-function renderInput() {
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     inputSection.value = null
-    for (value of myLeads) {
-        listItems = value
+    renderInput(myLeads)
+})
+function renderInput(leads) {
+    let listItems = ''
+    inputSection.value = null
+    for (i = 0; i < leads.length; i++) {
+        listItems += `<li> <a href = "${leads[i]}" target="_blank" >  ${leads[i]}  </a> </li>`
     }
-    document.getElementById("unorderedList").innerHTML +=
-        `<li> <a href = "${listItems}" target="_blank" >  ${listItems}  </a> </li>`
+    data.innerHTML = listItems
 }
+deleteItem.addEventListener("click", function () {
+    localStorage.clear()
+    myLeads = []
+    renderInput(myLeads)
+})
+saveURL.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderInput(myLeads)
+    })
+})
